13f743960251c9ef37b14cfd5547abe1541291e0
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-07-25 19:18:34 +0100

    Inherit Student class from Person class

--- library_classes.rb
+++ library_classes.rb
@@ -22,3 +22,21 @@ class Person
     @age >= 18
   end
 end
+
+class Student < Person
+  attr_reader :classroom
+  
+  def initialize(classroom, id, age, name = 'Unknown', parent_permission: true)
+    super(id, age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    return "¯\(ツ)/¯"
+  end
+end
+
+ahmed = Student.new("class1", "id01", 17, "otmane", parent_permission: false)
+
+puts ahmed.can_use_services?
+puts ahmed.play_hooky
