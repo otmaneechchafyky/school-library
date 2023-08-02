fff2bb6c31fdd630b23e65bc2e159b5ccc8c2ca8
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-08-02 04:00:33 +0100

    Fix linters

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-
 require_relative 'teacher'
 require_relative 'student'
 require_relative 'classroom'
@@ -6,7 +5,7 @@ require_relative 'rental'
 require_relative 'book'
 
 class App
-  def initialize()
+  def initialize
     @people = []
     @books = []
     @rentals = []
@@ -14,24 +13,24 @@ class App
 
   def list_all_books
     if @books.empty?
-      puts 'There is no books yet, add some please!'
+      puts 'There are no books yet, add some please!'
     else
       book_info = @books.map do |book|
         "Title: \"#{book.title}\", Author: \"#{book.author}\""
       end
+      puts book_info
     end
-    puts book_info
   end
 
   def list_all_people
     if @people.empty?
-      puts 'There is no people yet, please add a person!'
+      puts 'There are no people yet, please add a person!'
     else
       people_info = @people.map do |person|
         "[#{person.type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
+      puts people_info
     end
-    puts people_info
   end
 
   def create_a_person(choice, name, age)
@@ -42,7 +41,7 @@ class App
       person = create_a_teacher(name, age)
       person.type = 'Teacher'
     else
-      puts 'Uncorrect inputs!'
+      puts 'Incorrect input!'
     end
     @people.push(person)
     puts 'The person created successfully'
@@ -54,18 +53,18 @@ class App
     puts 'The Book created successfully'
   end
 
-  def create_a_rental()
-    puts '
-Select a book from list by number'
+  def create_a_rental
+    puts 'Select a book from the list by number:'
     list_of_books
     index_book = gets.chomp.to_i
-    puts '
-Select a person from list by number [not id]'
+
+    puts 'Select a person from the list by number [not id]:'
     list_of_people
     index_person = gets.chomp.to_i
-    print '
-Date: '
+
+    print 'Date: '
     date = gets.chomp
+
     rental = Rental.new(date, @books[index_book], @people[index_person])
     @rentals.push(rental)
     puts 'The Rental created successfully'
@@ -76,12 +75,10 @@ Date: '
     id = gets.chomp.to_i
     puts 'Rentals: '
     if @rentals.empty?
-      puts "There is no rentals for #{id}"
+      puts "There are no rentals for #{id}"
     else
       @rentals.each do |rental|
-        if rental.person.id == id
-          puts "Date: \"#{rental.date}\", Book: \"#{rental.book.title}\" wroten by: #{rental.book.author}"
-        end
+        puts "Date: \"#{rental.date}\", Book: \"#{rental.book.title}\" written by: #{rental.book.author}" if rental.person.id == id
       end
     end
   end
@@ -91,10 +88,9 @@ Date: '
   def create_a_student(name, age)
     print 'Parent permission? [Y/N]: '
     parent_decision = gets.chomp
-    print 'Class :'
+    print 'Class: '
     label = gets.chomp
-    parent_permission = true if %w[Y y].include?(parent_decision)
-    parent_permission = false if %w[N n].include?(parent_decision)
+    parent_permission = %w[Y y].include?(parent_decision)
     classroom = Classroom.new(label)
     Student.new(classroom, age, name, parent_permission: parent_permission)
   end
