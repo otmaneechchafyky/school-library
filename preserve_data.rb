dd9f249faed4a88a66b7d57dff3dbb3df60c99f2
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-08-02 03:40:41 +0100

    Create people.json file and save students and teachers inside

--- preserve_data.rb
+++ preserve_data.rb
@@ -7,4 +7,16 @@ class Save
                                                                               { 'title' => book.title, 'author' => book.author }
                                                                             end }))
   end
+
+  def save_people(people)
+    File.write('./json_files/people.json', JSON.pretty_generate({ 'People' => people.map do |person|
+                                                                                if person.instance_of?(Student)
+                                                                                  { 'type' => person.class, 'age' => person.age, 'name' => person.name,
+                                                                                    'parent_permission' => person.parent_permission, 'id' => person.id }
+                                                                                else
+                                                                                  { 'type' => person.class, 'age' => person.age, 'name' => person.name, 'specialization' => person.specialization,
+                                                                                    'id' => person.id }
+                                                                                end
+                                                                              end }))
+  end
 end
